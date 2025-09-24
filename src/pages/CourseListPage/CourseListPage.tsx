d989c217d502291342d28aea2d0e9c22445af827
yangjihun <yjhn0410@gmail.com>
2025-09-24 23:35:10 +0900

    chore: switch to mock data for course detail page - PIT-161

--- src/pages/CourseListPage/CourseListPage.tsx
+++ src/pages/CourseListPage/CourseListPage.tsx
@@ -2,10 +2,12 @@ import { Button } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComment } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import courseList from "../../features/Course/mocks/courseList.json";
 
 export const CourseListPage = () => {
-  
   const navigate = useNavigate();
+  const mockData = courseList.data.reviews;
+
   return (
     <div className="w-[800px] h-[calc(100vh-64px)]">
       <div className="flex flex-col gap-4 p-4 pt-0">
@@ -18,23 +20,25 @@ export const CourseListPage = () => {
           </div>
           <div className="grid grid-cols-3 grid-rows-3 gap-4">
             {
-              [1, 2, 3, 4, 5].map((item) => (
+              mockData.map((item) => (
                 <div className="flex flex-col gap-2 h-[320px] border-gray-300 border rounded-2xl pb-6 cursor-pointer"
-                  onClick={() => {navigate(`/course/${item}`)}} // 코스 상세 페이지로 이동
+                  onClick={() => {navigate(`/course/${item.id}`)}} // 코스 상세 페이지로 이동
                 >
                   <div className="flex gap-2">
                     <div className="w-full h-[165px] bg-gray-200 rounded-t-2xl"></div>
                   </div>
                   <div className="flex flex-col gap-2">
-                    <h2 className="px-4">15dfcd</h2>
-                    <p className="text-sm text-gray-500 px-2 py-4">드디어 함께 떠난 첫 여행! 제주도의 아름다운 바다와...</p>
+                    <h2 className="px-4">{item.id}</h2>
+                    <p className="text-sm text-gray-500 px-2 py-4">{item.comment}</p>
                   </div>
                   <div className="w-full flex justify-between px-2 gap-2 items-center">
                     <div className="w-[100px] text-sm">2025.09.16</div>
+                    {/* 추후에 댓글 기능 추가 시 사용
                     <div className="flex items-center w-[100px] flex justify-end gap-2 text-sm">
                       <FontAwesomeIcon icon={faComment} className="w-[12px] h-[12px]" />
                       3
                     </div>
+                    */}
                   </div>
                 </div>
               ))
