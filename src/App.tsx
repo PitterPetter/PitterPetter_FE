b63f18b48bcdcb1c20413e2a72cec7d661752b8b
yangjihun <yjhn0410@gmail.com>
2025-09-25 09:38:54 +0900

    chore: update comments in App.tsx - PIT-161

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { LoginPage } from './pages/LoginPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthInitializer } from './app/providers/AuthInitializer';
+// import { AuthInitializer } from './app/providers/AuthInitializer';
 import { MainPage } from './pages/MainPage';
 import { HeaderLayout } from './app/layouts';
 import { OptionsPage } from './pages/OptionsPage';
@@ -20,26 +20,38 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 로그인 페이지 */}
         <Route path="/login" element={<LoginPage />} />
+
+        {/* 헤더 레이아웃 */}
         <Route element={<HeaderLayout />}>
-          <Route path="/onboarding" element={<OnboardingPage />} />
           <Route path="/" element={<MainPage />} />
+          <Route path="/onboarding" element={<OnboardingPage />} />
           <Route path="/options" element={<OptionsPage />} />
+
+          {/* 커플 룸 페이지 */}
           <Route path="/coupleroom" element={<CoupleRoomPage />} />
           <Route path="/coupleroom/create" element={<CreateCoupleRoom />} />
           <Route path="/coupleroom/enter" element={<EnterCoupleRoom />} />
+
+          {/* 코스 추천 페이지 */}
           <Route path="/recommend" element={<RecommendCoursePage />}>
             <Route index element={<RecommendCourseSidebar />} />
             <Route path="course/:id" element={<RecommendPlaceSidebar />} />
           </Route>
 
+          {/* 코스 상세 페이지 */}
           <Route path="/course/:id" element={<CourseDetailPage />}>
             <Route index element={<CourseDetailSidebar />} />
             <Route path="place/:id" element={<PlaceDetailSidebar />} />
           </Route>
+
+          {/* 사이드바 페이지 */}
           <Route element={<SidebarLayout />}>
             <Route path="/course" element={<CourseListPage />} />
             <Route path="/mypage" element={<MyPage />} />
+
+            {/* 다이어리 페이지 */}
             <Route path="/diary" element={<DiaryListPage />} />
             <Route path="/diary/:id" element={<DiaryDetailPage />} />
           </Route>
