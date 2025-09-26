f9a358cee930b2de49e8cbfa1a5148f75a248b94
yangjihun <yjhn0410@gmail.com>
2025-09-26 19:01:14 +0900

    feat: redirect to backend google server - PIT-212

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,27 +1,15 @@
 import { Mapbox } from "../../shared/ui/Mapbox";
 import { Button } from "../../shared/ui/button";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
-  const [isClick, setIsClick] = useState(false);
-  const navigation = useNavigate();
-  useEffect(() => {
-    if (isClick) {
-      sessionStorage.setItem("token", "hi");
-      if (sessionStorage.getItem("token") !== "firstLogin") {
-        navigation("/");
-      } else {
-        navigation("/onboarding");
-      }
-    }
-  }, [isClick]);
+
   return (
     <div className="relative w-full h-full flex">
       <div className="flex flex-col justify-center items-center min-w-[420px] h-full">
         <div className="text-sm text-gray-500">LoginPage</div>
         <div className="text-2xl font-bold">로그인</div>
-        <Button className="text-sm text-gray-500" onClick={() => setIsClick(true)}>구글로 로그인</Button>
+        {/* 백엔드 구글 서버로 리다이랙트 */}
+        <Button className="text-sm text-gray-500" onClick={() => window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`}>구글로 로그인</Button>
       </div>
       <Mapbox />
     </div>
