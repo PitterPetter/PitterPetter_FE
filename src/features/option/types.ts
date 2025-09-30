7f920e90542b6cd45c300e871bb284dab08e5971
yangjihun <yjhn0410@gmail.com>
2025-09-30 16:06:04 +0900

    feat: add start time & end time to optionsPage - PIT-290

--- src/features/option/types.ts
+++ src/features/option/types.ts
@@ -3,4 +3,6 @@ export type Option = {
   condition: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
   drink_intent: boolean;
   food: string;
+  startTime: Date;
+  endTime: Date;
 };
--- src/pages/OptionsPage/OptionsPage.tsx
+++ src/pages/OptionsPage/OptionsPage.tsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Slider } from "@mui/material";
-import { Button } from "@mui/material";
-import { TextField } from "@mui/material";
+import { Slider, Button, TextField } from "@mui/material";
 import { useMutation } from "@tanstack/react-query";
 import { postOption } from "../../features/option/api";
 import { Option } from "./type";
-import { useRecommendStore } from "../../shared/store/recommend.store";
-import { useStartStore } from "../../shared/store/recommend.store";
+import { useRecommendStore, useStartStore } from "../../shared/store/recommend.store";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { renderTimeViewClock } from "@mui/x-date-pickers/timeViewRenderers";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
 export const OptionsPage = () => {
   const navigation = useNavigate();
   const [condition, setCondition] = useState<Option['user_choice']['condition']>(5);
   const [drink_intent, setDrinking] = useState<Option['user_choice']['drink_intent']>(false);
   const [food, setFood] = useState<Option['user_choice']['food']>("");
+  const [startTime, setStartTime] = useState<Date>(new Date());
+  const [endTime, setEndTime] = useState<Date>(new Date());
   const start = useStartStore.getState();
   const mutation = useMutation({
     mutationFn: postOption,
@@ -27,15 +30,45 @@ export const OptionsPage = () => {
     }
   });
   const handleSubmit = () => {
-    console.log({ user_choice: { start: [start.lat, start.lng], condition, drink_intent, food } });
-    mutation.mutate({ user_choice: { start: [start.lat, start.lng], condition, drink_intent, food } });
+    console.log({ user_choice: { start: [start.lat, start.lng], condition, drink_intent, food, startTime, endTime } });
+    mutation.mutate({ user_choice: { start: [start.lat, start.lng], condition, drink_intent, food, startTime, endTime } });
   };
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center px-80">
-      <h1 className="text-2xl font-bold py-4">Set Your Prefencences</h1>
-      <p className="text-gray-500 pb-8">원하는 조건을 설정해주세요.</p>
-      <div className="w-full flex flex-col gap-8 px-40">
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <div className="w-full h-full flex flex-col items-center justify-center px-80">
+        <h1 className="text-2xl font-bold py-4">Set Your Prefencences</h1>
+        <p className="text-gray-500 pb-8">원하는 조건을 설정해주세요.</p>
+        <div className="w-full flex flex-col gap-8 px-40">
+
+        {/* Start */}
+        <div className="flex flex-col gap-2 w-full pb-4">
+          <div className="flex flex-col gap-2 w-full">
+            <h1 className="text-gray-500">Start</h1>
+            <TimePicker
+              label="Start Time"
+              value={startTime}
+              onChange={(e) => setStartTime(e as Date)}
+              viewRenderers={{
+                hours: renderTimeViewClock,
+                minutes: renderTimeViewClock,
+                seconds: renderTimeViewClock,
+              }}
+            />
+            <h1 className="text-gray-500">End</h1>
+            <TimePicker
+              label="End Time"
+              value={endTime}
+              onChange={(e) => setEndTime(e as Date)}
+              viewRenderers={{
+                hours: renderTimeViewClock,
+                minutes: renderTimeViewClock,
+                seconds: renderTimeViewClock,
+              }}
+            />
+          </div>
+        </div>
+
         {/* Condition */}
         <div className="flex flex-col gap-2 w-full pb-4 justify-center">
           <h1 className="text-gray-500">Condition</h1>
@@ -65,7 +98,8 @@ export const OptionsPage = () => {
           <p className="text-gray-500">옵션을 비워도 추천이 가능합니다</p>
         </div>
       </div>
-    </div>
+      </div>
+    </LocalizationProvider>
   );
 };
 
