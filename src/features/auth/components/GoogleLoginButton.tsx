280cc794c636a67d1d5a17eefa6a1b214ec12a15
yangjihun <yjhn0410@gmail.com>
2025-10-03 01:34:15 +0900

    style: change button variant from contained to outlined in social login button - PIT-336

--- src/features/auth/components/GoogleLoginButton.tsx
+++ src/features/auth/components/GoogleLoginButton.tsx
@@ -7,8 +7,8 @@ export const GoogleLoginButton = () => {
   return (
     <div className="flex items-center justify-center">
     {/* 백엔드 구글 서버로 리다이랙트 */}
-    <Button variant="contained" className="w-[160px] text-sm text-gray-500" onClick={() => window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`}>
-      <FontAwesomeIcon icon={faGoogle as any} className="mr-2 text-white" />
+    <Button variant="outlined" className="w-[160px] text-sm text-gray-500" onClick={() => window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`}>
+      <FontAwesomeIcon icon={faGoogle as any} className="mr-2 text-blue-500" />
       구글로 로그인
     </Button>
     </div>
