8c8c61e007b7f7986d16e4747e5e9ee9f7508044
yangjihun <yjhn0410@gmail.com>
2025-10-03 01:28:35 +0900

    chore: increase width of Google login button - PIT-336

--- src/features/auth/components/GoogleLoginButton.tsx
+++ src/features/auth/components/GoogleLoginButton.tsx
@@ -7,7 +7,7 @@ export const GoogleLoginButton = () => {
   return (
     <div className="flex items-center justify-center">
     {/* 백엔드 구글 서버로 리다이랙트 */}
-    <Button variant="contained" className="text-sm text-gray-500" onClick={() => window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`}>
+    <Button variant="contained" className="w-[160px] text-sm text-gray-500" onClick={() => window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`}>
       <FontAwesomeIcon icon={faGoogle as any} className="mr-2 text-white" />
       구글로 로그인
     </Button>
