28bf91f43c14cb73a10ac4af381b1fc91012ee64
yangjihun <yjhn0410@gmail.com>
2025-10-03 22:40:10 +0900

    style: update text content for better readability in onboardingPage - PIT-375

--- src/pages/OnboardingPage/OnboardingPage.tsx
+++ src/pages/OnboardingPage/OnboardingPage.tsx
@@ -19,27 +19,27 @@ export const OnboardingPage = () => {
         <div className="h-full p-4 pb-6">
           {/* 개인 온보딩 */}
           <div className="mt-4 flex flex-col gap-8 items-center justify-center">
-            <h1 className="text-xl font-bold py-8">Set Your Prefencences</h1>
-            <p className="text-gray-500">본인의 성향을 설정해주세요</p>
+            <h1 className="text-xl font-bold py-8">혜준 님의 취향을 알려주세요</h1>
+            <p className="text-gray-500">정보를 입력해 주시면 더 정확한 추천을 해드릴 수 있어요</p>
             <div className="flex flex-col gap-8 px-20">
               <div className="w-full flex flex-col gap-2">
-                <p className="text-gray-500">술을 좋아하시나요?</p>
+                <p className="text-gray-500">술을 즐기는 걸 선호한다.</p>
                 <div className="flex gap-2">
-                  <p className="text-gray-500">No</p>
+                  <p className="text-gray-500">그렇지 않다</p>
                   <Slider defaultValue={5} aria-label="Temperature" valueLabelDisplay="auto" min={0} max={10} />
-                  <p className="text-gray-500">Yes</p>
+                  <p className="text-gray-500">그렇다</p>
                 </div>
               </div>
               <div>
-                <p className="text-gray-500">활동적이시나요?</p>
+                <p className="text-gray-500">활동적인 데이트를 선호한다.</p>
               </div>
               <div className="flex gap-2">
-                <p>No</p>
+                <p>그렇지 않다</p>
                 <Slider defaultValue={5} aria-label="Temperature" valueLabelDisplay="auto" min={0} max={10} />
-                <p>Yes</p>
+                <p>그렇다</p>
               </div>
               <div className="w-full flex flex-col gap-2">
-                <p className="text-gray-500">좋아하는 음식</p>
+                <p className="text-gray-500">선호하는 음식은 무엇인가요?</p>
                 <div className="flex gap-2 w-full justify-between w-full">
                   {foodList.map((f) => {
                     const selected = food.includes(f);
@@ -62,18 +62,18 @@ export const OnboardingPage = () => {
                 </div>
               </div>
               <div className="w-full flex flex-col gap-2">
-                <p className="text-gray-500">평소 데이트 선호 비용은 얼마인가요?</p>
+                <p className="text-gray-500">데이트 비용은 얼마를 선호하시나요?</p>
                 <div className="flex gap-2 w-full justify-between w-full">
                   <Button variant={cost==='1' ? "contained" : "outlined"} className="w-full whitespace-nowrap" onClick={() => setCost('1')}>1만원 이하</Button>
-                  <Button variant={cost==='2' ? "contained" : "outlined"} className="w-full whitespace-nowrap" onClick={() => setCost('2')}>1만원 ~ 3만원</Button>
-                  <Button variant={cost==='3' ? "contained" : "outlined"} className="w-full whitespace-nowrap" onClick={() => setCost('3')}>3만원 ~ 5만원</Button>
-                  <Button variant={cost==='4' ? "contained" : "outlined"} className="w-full whitespace-nowrap" onClick={() => setCost('4')}>5만원 ~ 8만원</Button>
+                  <Button variant={cost==='2' ? "contained" : "outlined"} className="w-full whitespace-nowrap" onClick={() => setCost('2')}>1 ~ 3만원</Button>
+                  <Button variant={cost==='3' ? "contained" : "outlined"} className="w-full whitespace-nowrap" onClick={() => setCost('3')}>3 ~ 5만원</Button>
+                  <Button variant={cost==='4' ? "contained" : "outlined"} className="w-full whitespace-nowrap" onClick={() => setCost('4')}>5 ~ 8만원</Button>
                   <Button variant={cost==='5' ? "contained" : "outlined"} className="w-full whitespace-nowrap" onClick={() => setCost('5')}>8만원 이상</Button>
                 </div>
               </div>
               <div className="w-full flex flex-col gap-2">
-                <p className="text-gray-500">어떤 분위기를 선호하시나요?</p>
-                <TextField id="outlined-basic" variant="outlined" placeholder="입력해주세요" />
+                <p className="text-gray-500">어떤 분위기를 선호하세요?</p>
+                <TextField id="outlined-basic" variant="outlined" placeholder="입력해주세요." />
               </div>
             </div>
           </div>
