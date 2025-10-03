f7bb63efc46fdd72c7d1a33a9d927c7d29b76dc2
yangjihun <yjhn0410@gmail.com>
2025-10-03 21:54:50 +0900

    style: adjust social login button in loginpage - PIT-336

--- src/features/auth/components/GoogleLoginButton.tsx
+++ src/features/auth/components/GoogleLoginButton.tsx
@@ -1,15 +1,26 @@
 import { Button } from "@mui/material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import googleIcon from "../assets/googleLogo.png";
 
 
 export const GoogleLoginButton = () => {
   return (
     <div className="flex items-center justify-center">
     {/* 백엔드 구글 서버로 리다이랙트 */}
-    <Button variant="outlined" className="w-[160px] text-sm text-gray-500" onClick={() => window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`}>
-      <FontAwesomeIcon icon={faGoogle as any} className="mr-2 text-blue-500" />
-      구글로 로그인
+    <Button
+      variant="outlined"
+      sx={{
+        borderRadius: '20px',
+        width: '240px',
+        height: '40px',
+        textAlign: 'center',
+        textTransform: 'none',
+        fontSize: '14px',
+        color: 'rgb(55, 53, 47)',
+      }}
+      onClick={() => window.location.href = `${import.meta.env.VITE_API_BASE_URL}/oauth2/authorization/google`}
+    >
+      <img src={googleIcon} alt="google" className="w-6 h-6 mr-2" />
+      Google로 로그인
     </Button>
     </div>
   )
