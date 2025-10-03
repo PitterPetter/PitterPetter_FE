d3cb6b181f8afb2475e8a3bdc99b5cb71ed3dd8c
yangjihun <yjhn0410@gmail.com>
2025-10-03 22:09:17 +0900

    style: adjust gap in loginPage - PIT-336

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -7,9 +7,10 @@ export const LoginPage = () => {
   return (
     <div className="relative w-full h-[100vh] flex justify-center items-center">
       <div className="
-        w-full 2xl:w-[1400px] h-full z-40 flex flex-col justify-center items-center gap-10 bg-white/80 2xl:rounded-2xl px-10
-        xl:h-[700px] xl:px-20
-        lg:h-[700px] lg:flex-row lg:gap-20
+        w-full h-full z-40 flex flex-col justify-center items-center bg-white/80 2xl:rounded-2xl px-20
+        2xl:w-[1400px]
+        xl:h-[700px]
+        lg:h-[700px] lg:flex-row
       ">
         <div className="w-full h-full z-40 flex justify-center items-center">
           <PhotoStack />
