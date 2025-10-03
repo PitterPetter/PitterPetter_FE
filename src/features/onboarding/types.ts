4780eeec538b075d5f55ff900a6fb8615f4ad93b
yangjihun <yjhn0410@gmail.com>
2025-10-04 02:20:38 +0900

    chore: add AtmosphereList type in onboarding types - PIT-375

--- src/features/onboarding/types.ts
+++ src/features/onboarding/types.ts
@@ -2,11 +2,12 @@ export type Onboarding = {
     drinking: DrinkingList;
     active: ActiveList;
     cost: CostList;
-    food: FoodList;
+    food: FoodList[];
+    atmosphere: AtmosphereList;
 };
 
 export type DrinkingList = 0 | 1 | 2 | 3 | 4 | 5; // drinking category
 export type ActiveList = 0 | 1 | 2 | 3 | 4 | 5; // active category
-export type FoodList = ['한식', '중식', '양식', '일식', '분식']; // food category
+export type FoodList = '한식' | '중식' | '양식' | '일식' | '분식'; // food category
 export type CostList = '' | '1만원 이하' | '1 ~ 3만원' | '3 ~ 5만원' | '5 ~ 8만원' | '8만원 이상'; // cost category
 export type AtmosphereList = string; // atmosphere category
