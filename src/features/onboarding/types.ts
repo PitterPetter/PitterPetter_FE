8f1dc9135fff6e05685330b6cdc53d1a090ad76e
yangjihun <yjhn0410@gmail.com>
2025-10-04 01:58:36 +0900

    refactor: move onboarding type from pages to features - PIT-375

---
+++ src/features/onboarding/types.ts
@@ -0,0 +1,13 @@
+export type Onboarding = {
+  user_choice: {
+    drinking: DrinkingList;
+    active: ActiveList;
+    cost: CostList;
+    food: FoodList;
+  };
+}
+
+export type DrinkingList = 0 | 1 | 2 | 3 | 4 | 5; // drinking category
+export type ActiveList = 0 | 1 | 2 | 3 | 4 | 5; // active category
+export type FoodList = ['한식', '중식', '양식', '일식', '분식']; // food category
+export type CostList = '' | '1만원 이하' | '1 ~ 3만원' | '3 ~ 5만원' | '5 ~ 8만원' | '8만원 이상'; // cost category
--- src/pages/OnboardingPage/types.ts
+++
@@ -1,4 +0,0 @@
-export type DrinkingList = 0 | 1 | 2 | 3 | 4 | 5; // drinking category
-export type ActiveList = 0 | 1 | 2 | 3 | 4 | 5; // active category
-export type FoodList = ['한식', '중식', '양식', '일식', '분식']; // food category
-export type CostList = '' | '1만원 이하' | '1 ~ 3만원' | '3 ~ 5만원' | '5 ~ 8만원' | '8만원 이상'; // cost category
