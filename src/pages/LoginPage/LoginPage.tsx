468eee072c4cc9e38664d988c0a6f9b02b00c9bd
yangjihun <yjhn0410@gmail.com>
2025-10-03 21:10:12 +0900

    feat: set up login page structure in LoginPage - PIT-336

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,16 +1,27 @@
+import { PhotoStack } from "../../features/auth/components/PhotoStack";
+import { LoginSubmit } from "../../features/auth/components/LoginSubmit";
 import { LoginMapbox } from "../../features/mapbox";
-import { GoogleLoginButton } from "../../features/auth";
 
 export const LoginPage = () => {
 
   return (
-    <div className="relative w-full h-full flex">
-      <div className="flex flex-col justify-center items-center min-w-[420px] h-full">
-        <div className="text-sm text-gray-500">LoginPage</div>
-        <div className="text-2xl font-bold">로그인</div>
-        <GoogleLoginButton />
+    <div className="relative w-full h-[100vh] flex justify-center items-center">
+      <div className="
+        w-full 2xl:w-[1400px] h-full z-40 flex flex-col justify-center items-center gap-10 bg-white/80 2xl:rounded-2xl px-10
+        xl:h-[700px] xl:px-20
+        lg:h-[700px] lg:flex-row lg:gap-20
+      ">
+        <div className="w-full h-full z-40 flex justify-center items-center">
+          <PhotoStack />
+        </div>
+        <div className="w-full h-full z-40 flex justify-center items-center">
+          <LoginSubmit />
+        </div>
       </div>
-      <LoginMapbox />
+      {/* background */}
+      <div className="absolute top-0 left-0 w-full max-h-[100vh] z-10 flex justify-center items-center">
+        <LoginMapbox />
+      </div>
     </div>
   );
 };
