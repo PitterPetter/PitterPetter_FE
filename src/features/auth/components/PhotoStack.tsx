085291f1adda6859205f472a577f0b384b8128e7
yangjihun <yjhn0410@gmail.com>
2025-10-05 13:18:09 +0900

    style: replace photo UI in PhotoStack - PIT-389

--- src/features/auth/components/PhotoStack.tsx
+++ src/features/auth/components/PhotoStack.tsx
@@ -5,49 +5,57 @@ import lotteWorld from "../assets/lotteWorld.png";
 import shineIcon from "../../../shared/ui/assets/shineIcon.png";
 import pageIcon from "../../../shared/ui/assets/pageIcon.png";
 import bookIcon from "../../../shared/ui/assets/bookIcon.png";
+import logo from "/logo.png";
 
 export const PhotoStack = () => {
-  const imageStyle = "flex p-[3px] bg-white hover:scale-105 transition-all duration-250 rounded-[24px]";
-  const buttonStyle = "absolute bg-white/30 backdrop-blur-sm border border-white/20 shadow-xl flex items-center justify-center rounded-[80px] p-[3px] hover:scale-105 transition-all duration-250";
+  const imageStyle = "w-[200px] h-[136px] lg:w-[250px] lg:h-[160px] flex p-[3px] bg-white transition-all duration-500 rounded-[24px]";
+  const buttonStyle = "absolute bg-white/30 backdrop-blur-sm border border-white/20 shadow-xl flex items-center justify-center rounded-[80px] p-[3px] transition-all duration-500";
 
   return (
-    <div className="relative flex flex-col justify-center items-center z-0 p-4 w-[340px] h-[300px]
-      lg:w-[470px] lg:h-[360px]
+    <div className="relative flex flex-col justify-center items-center z-0 p-4 w-[400px] h-[300px] group
+      lg:w-[800px] lg:h-[600px]
     ">
+      <img src={logo} alt="logo" className="w-14 h-14" />
       {/* image */}
       <div className={`
         ${imageStyle}        
-        absolute top-0 left-15 z-10 rotate-[1deg] w-[200px] h-[136px]
-        lg:w-[250px] lg:h-[166px] lg:left-15 lg:top-[-18px]
+        absolute top-[0px] left-[100px] z-10 rotate-[1deg]
+        group-hover:left-[0px] group-hover:top-[0px] group-hover:rotate-[-5deg]
+        lg:group-hover:left-[210px] lg:group-hover:top-[50px] lg:group-hover:rotate-[-10deg]
+        lg:left-[240px] lg:top-[70px]
       `}>
         <img src={kyungbock} alt="kyungbock" className="w-full h-full rounded-[20px]" />
       </div>
       <div className={`
         ${imageStyle}
-        absolute top-20 right-5 z-20 rotate-[15deg] hover:rotate-[12deg] w-[200px] h-[136px]
-        lg:w-[250px] lg:h-[166px] lg:right-5 lg:top-24
+        absolute top-[90px] right-[5px] z-20 rotate-[15deg] group-hover:rotate-[12deg]
+        group-hover:right-[80px] group-hover:top-[180px] group-hover:rotate-[4deg]
+        lg:right-[240px] lg:top-[200px]
       `}>
         <img src={ddp} alt="ddp" className="w-full h-full rounded-[20px]" />
       </div>
       <div className={`
         ${imageStyle}
-        absolute top-20 left-3 z-30 rotate-[-5deg] hover:rotate-[-2deg] w-[200px] h-[136px]
-        lg:w-[250px] lg:h-[166px] lg:left-8 lg:top-28
+        absolute top-20 left-3 z-30 rotate-[-5deg]
+        group-hover:left-[100px] group-hover:top-[250px] group-hover:rotate-[8deg]
+        lg:left-[150px] lg:top-[220px]
       `}>
         <img src={lotteTower} alt="lotteTower" className="w-full h-full rounded-[20px]" />
       </div>
       <div className={`
         ${imageStyle}
-        absolute bottom-0 right-5 z-40 rotate-[4deg] hover:rotate-[1deg] w-[200px] h-[136px]
-        lg:w-[250px] lg:h-[166px] lg:right-5 lg:bottom-[-12px]
+        absolute bottom-0 right-5 z-40 rotate-[4deg] group-hover:rotate-[1deg]
+        group-hover:right-[180px] group-hover:bottom-[70px] group-hover:rotate-[-10deg]
+        lg:right-[240px] lg:bottom-[120px]
       `}>
         <img src={lotteWorld} alt="lotteWorld" className="w-full h-full rounded-[20px]" />
       </div>
       {/* button */}
       <div className={`
         ${buttonStyle}
-        absolute top-8 left-[-20px] z-20 rotate-[15deg] hover:rotate-[12deg] w-[168px] h-[48px]
-        lg:w-[200px] lg:h-[60px] lg:left-[-50px] lg:top-16
+        absolute top-8 left-[15px] z-20 rotate-[15deg] group-hover:rotate-[12deg] w-[168px] h-[48px]
+        group-hover:left-[30px] group-hover:rotate-[1deg]
+        lg:w-[200px] lg:h-[60px] lg:left-[70px] lg:top-[170px]
       `}>
         <div className="flex items-center justify-center bg-pink-100 w-full h-full rounded-[80px] gap-4">
           <img src={pageIcon} alt="book" className="w-[28px] h-[28px]" />
@@ -56,8 +64,9 @@ export const PhotoStack = () => {
       </div>
       <div className={`
         ${buttonStyle}
-        absolute bottom-12 left-[12px] z-50 rotate-[-15deg] hover:rotate-[-12deg] w-[204px] h-[48px]
-        lg:w-[250px] lg:h-[60px] lg:left-0 lg:bottom-8
+        absolute bottom-12 left-[12px] z-50 rotate-[-12deg] group-hover:rotate-[-12deg] w-[204px] h-[48px]
+        group-hover:left-[180px] group-hover:bottom-[160px]
+        lg:w-[250px] lg:h-[60px] lg:left-[170px] lg:bottom-[170px]
       `}>
         <div className="flex items-center justify-center bg-pink-100 w-full h-full rounded-[80px] gap-4">
           <img src={shineIcon} alt="shine" className="w-[28px] h-[28px]" />
@@ -66,8 +75,9 @@ export const PhotoStack = () => {
       </div>
       <div className={`
         ${buttonStyle}
-        absolute bottom-30 right-[-40px] z-30 rotate-[-2deg] hover:rotate-[1deg] w-[148px] h-[48px]
-        lg:w-[180px] lg:h-[60px] lg:right-[-80px] lg:bottom-32
+        absolute bottom-30 right-[0px] z-30 rotate-[-2deg] group-hover:rotate-[1deg] w-[148px] h-[48px]
+        group-hover:right-[30px] group-hover:bottom-[200px] group-hover:rotate-[15deg]
+        lg:w-[180px] lg:h-[60px] lg:right-[100px] lg:bottom-[260px]
       `}>
         <div className="flex items-center justify-center bg-pink-200 w-full h-full rounded-[80px] gap-4">
           <img src={bookIcon} alt="book" className="w-[28px] h-[28px]" />
