f6c3e91ed3bb465bae8929e3a64001c557c4dfac
yangjihun <yjhn0410@gmail.com>
2025-10-05 21:09:23 +0900

    chore: set up button navigation in CoupleRoomPage - PIT-393

--- src/pages/CoupleRoomPage/CoupleRoomPage.tsx
+++ src/pages/CoupleRoomPage/CoupleRoomPage.tsx
@@ -4,12 +4,12 @@ import { useNavigate } from "react-router-dom";
 export const CoupleRoomPage = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex items-center justify-center w-full h-[calc(100vh-64px)]">
-      <div className="h-[700px] w-[700px] border-gray-300 border rounded-2xl p-4 pb-6 flex flex-col gap-4 justify-center items-center">
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="h-[700px] w-[700px] bg-white border-gray-300 border rounded-2xl p-4 pb-6 flex flex-col gap-4 justify-center items-center">
         <h1 className="text-xl font-bold py-4">커플 방</h1>
         {/* navigation buttons */}
-        <Button variant="contained" onClick={() => navigate('/coupleroom/create')}>커플 방 생성</Button>
-        <Button variant="contained" onClick={() => navigate('/coupleroom/enter')}>커플 방 입장</Button>
+        <Button variant="contained" onClick={() => navigate('/home/coupleroom/create')}>커플 방 생성</Button>
+        <Button variant="contained" onClick={() => navigate('/home/coupleroom/enter')}>커플 방 입장</Button>
       </div>
     </div>
   );
