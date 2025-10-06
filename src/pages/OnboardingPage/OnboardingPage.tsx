7a93f82b5b6a2d02477f7a7ed22d3fbb0741ef48
yangjihun <yjhn0410@gmail.com>
2025-10-06 14:57:13 +0900

    chore: add personal onboarding text in OnboardingPage - PIT-401

--- src/pages/OnboardingPage/OnboardingPage.tsx
+++ src/pages/OnboardingPage/OnboardingPage.tsx
@@ -9,8 +9,10 @@ export const OnboardingPage = () => {
   return (
     <div className="w-full flex justify-center">
       <div className="flex flex-col gap-4 p-4 pt-0 max-w-[450px] md:max-w-[800px]">
-        <div className="h-full p-4 pb-6">
+        <div className="h-full p-4 pb-6 flex flex-col gap-4 items-center justify-center">
           {/* 개인 온보딩 */}
+          <h1 className="text-2xl">혜준 님의 취향을 알려주세요</h1>
+          <p className="text-gray-500 pb-8">정보를 입력해 주시면 더 정확한 추천을 해드릴 수 있어요</p>
           <PersonalOnboarding />
           {/* Button - API 연결 후 Post하고 메인 페이지로 이동 추가 예정 */}
           <div className="flex justify-center items-center mt-12">
