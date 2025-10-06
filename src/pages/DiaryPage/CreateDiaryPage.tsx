861461d897d7a6b0dffbc57db1a130934a62af48
yangjihun <yjhn0410@gmail.com>
2025-10-06 21:16:33 +0900

    feat: add ConnectCourse component in CreateDiaryPage - PIT-413

---
+++ src/pages/DiaryPage/CreateDiaryPage.tsx
@@ -0,0 +1,26 @@
+import { ConnectCourse } from "../../features/diary/components/ConnectCourse";
+
+export const CreateDiaryPage = () => {
+  
+  return (
+    <div className="w-full h-[100vh] flex flex-col gap-4 items-center justify-center">
+      {/* 코스 연결 */}
+      <ConnectCourse />
+
+      {/* 리뷰 */}
+      <div className="h-full border-gray-300 border rounded-2xl p-4 pb-6 w-[800px]">
+
+      </div>
+
+      {/* 게시물 작성 */}
+      <div className="h-full border-gray-300 border rounded-2xl p-4 pb-6 w-[800px]">
+
+      </div>
+
+      {/* 저장 */}
+      <div className="h-full border-gray-300 border rounded-2xl p-4 pb-6 w-[800px]">
+
+      </div>
+    </div>
+  );
+};
