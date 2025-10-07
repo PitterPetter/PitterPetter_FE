7dd950ccfeda69ad005ffb8643581a12329d8996
yangjihun <yjhn0410@gmail.com>
2025-10-08 02:58:51 +0900

    refactor: refactoring Personal Onboarding page - PIT-434

--- src/features/onboarding/PersonalOnboarding.tsx
+++ src/features/onboarding/PersonalOnboarding.tsx
@@ -5,11 +5,14 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import { FoodList, CostList, DrinkingList, ActiveList } from "./types";
 
+const FOOD_CATEGORIES = ['한식', '중식', '양식', '일식', '분식'];
+const COST_PREFERENCE = ['1만원 이하', '1 ~ 3만원', '3 ~ 5만원', '5 ~ 8만원', '8만원 이상'];
+
 export const PersonalOnboarding = () => {
   const [alcoholPreference, setAlcoholPreference] = useState<DrinkingList>(0);
   const [activeBound, setActiveBound] = useState<ActiveList>(0);
   const [dataCostPreference, setDataCostPreference] = useState<CostList>('');
-  const [favoriteFoodCategories, setFavoriteFoodCategories] = useState<FoodList[number][]>([]);
+  const [favoriteFoodCategories, setFavoriteFoodCategories] = useState<FoodList[]>([]);
   const circleStyle = "border border-gray-300 rounded-full transition-all duration-250 flex justify-center items-center text-white";
   const boxStyle = "w-full h-12 rounded-md transition-all duration-250 flex justify-center items-center border border-gray-300";
   
@@ -23,57 +26,34 @@ export const PersonalOnboarding = () => {
         {/* Drinking */}
         <div className="w-full flex flex-col gap-4">
           <p className="text-black">술을 즐기는 걸 선호한다.</p>
-          <div className="
-            w-full
-            grid grid-cols-2 items-center gap-4
-            md:flex md:justify-between md:items-center
-          ">
-            <div className="
-              col-span-2 order-2
-              flex justify-between items-center gap-4
-              md:gap-12
-            ">
+          <div className="w-full grid grid-cols-2 items-center gap-4 md:flex md:justify-between md:items-center">
+            <div className="col-span-2 order-2 flex justify-between items-center gap-4 md:gap-12">
               <span
-                className={`
-                  w-12 h-12 ${circleStyle} ${alcoholPreference === 1 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-16 md:h-16
-                `}
+                className={`w-12 h-12 ${circleStyle} ${alcoholPreference === 1 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-16 md:h-16`}
                 onClick={() => setAlcoholPreference(1)}
               >
                 {alcoholPreference===1 ? <FontAwesomeIcon icon={faCheck} /> : ""}
               </span>
               <span
-                className={`
-                  w-10 h-10 ${circleStyle} ${alcoholPreference === 2 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-14 md:h-14
-                `}
+                className={`w-10 h-10 ${circleStyle} ${alcoholPreference === 2 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-14 md:h-14`}
                 onClick={() => setAlcoholPreference(2)}
               >
                 {alcoholPreference===2 ? <FontAwesomeIcon icon={faCheck} /> : ""}
               </span>
               <span
-                className={`
-                  w-8 h-8 ${circleStyle} ${alcoholPreference === 3 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-12 md:h-12
-                `}
+                className={`w-8 h-8 ${circleStyle} ${alcoholPreference === 3 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-12 md:h-12`}
                 onClick={() => setAlcoholPreference(3)}
               >
                 {alcoholPreference===3 ? <FontAwesomeIcon icon={faCheck} /> : ""}
               </span>
               <span
-                className={`
-                  w-10 h-10 ${circleStyle} ${alcoholPreference === 4 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-14 md:h-14
-                `}
+                className={`w-10 h-10 ${circleStyle} ${alcoholPreference === 4 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-14 md:h-14`}
                 onClick={() => setAlcoholPreference(4)}
               >
                 {alcoholPreference===4 ? <FontAwesomeIcon icon={faCheck} /> : ""}
               </span>
               <span
-                className={`
-                  w-12 h-12 ${circleStyle} ${alcoholPreference === 5 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-16 md:h-16
-                `}
+                className={`w-12 h-12 ${circleStyle} ${alcoholPreference === 5 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-16 md:h-16`}
                 onClick={() => setAlcoholPreference(5)}
               >
                 {alcoholPreference===5 ? <FontAwesomeIcon icon={faCheck} /> : ""}
@@ -86,57 +66,34 @@ export const PersonalOnboarding = () => {
         {/* Active */}
         <div className="w-full flex flex-col gap-4">
           <p className="text-black">활동적인 데이트를 선호한다.</p>
-          <div className="
-            w-full
-            grid grid-cols-2 items-center gap-2
-            md:flex md:justify-between md:items-center
-          ">
-            <div className="
-              col-span-2 order-2
-              flex justify-between items-center gap-4
-              md:gap-12
-            ">
+          <div className="w-full grid grid-cols-2 items-center gap-2 md:flex md:justify-between md:items-center">
+            <div className="col-span-2 order-2 flex justify-between items-center gap-4 md:gap-12">
               <span
-                className={`
-                  w-12 h-12 ${circleStyle} ${activeBound === 1 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-16 md:h-16
-                `}
+                className={`w-12 h-12 ${circleStyle} ${activeBound === 1 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-16 md:h-16`}
                 onClick={() => setActiveBound(1)}
               >
                 {activeBound===1 ? <FontAwesomeIcon icon={faCheck} /> : ""}
               </span>
               <span
-                className={`
-                  w-10 h-10 ${circleStyle} ${activeBound === 2 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-14 md:h-14
-                `}
+                className={`w-10 h-10 ${circleStyle} ${activeBound === 2 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-14 md:h-14`}
                 onClick={() => setActiveBound(2)}
               >
                 {activeBound===2 ? <FontAwesomeIcon icon={faCheck} /> : ""}
               </span>
               <span
-                className={`
-                  w-8 h-8 ${circleStyle} ${activeBound === 3 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-12 md:h-12
-                `}
+                className={`w-8 h-8 ${circleStyle} ${activeBound === 3 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-12 md:h-12`}
                 onClick={() => setActiveBound(3)}
               >
                 {activeBound===3 ? <FontAwesomeIcon icon={faCheck} /> : ""}
               </span>
               <span
-                className={`
-                  w-10 h-10 ${circleStyle} ${activeBound === 4 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-14 md:h-14
-                `}
+                className={`w-10 h-10 ${circleStyle} ${activeBound === 4 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-14 md:h-14`}
                 onClick={() => setActiveBound(4)}
               >
                 {activeBound===4 ? <FontAwesomeIcon icon={faCheck} /> : ""}
               </span>
               <span
-                className={`
-                  w-12 h-12 ${circleStyle} ${activeBound === 5 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"}
-                  md:w-16 md:h-16
-                `}
+                className={`w-12 h-12 ${circleStyle} ${activeBound === 5 ? "bg-[#93000A]/60" : "hover:bg-[#93000A]/40"} md:w-16 md:h-16`}
                 onClick={() => setActiveBound(5)}
               >
                 {activeBound===5 ? <FontAwesomeIcon icon={faCheck} /> : ""}
@@ -150,22 +107,18 @@ export const PersonalOnboarding = () => {
         <div className="w-full flex flex-col gap-2">
           <p className="text-black">선호하는 음식은 무엇인가요?</p>
           <div className="flex gap-2 w-full justify-between w-full">
-            <span className={`${boxStyle} ${favoriteFoodCategories.includes('한식') ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setFavoriteFoodCategories((prev) => prev.includes('한식') ? prev.filter((x) => x !== '한식') : [...prev, '한식'])}>한식</span>
-            <span className={`${boxStyle} ${favoriteFoodCategories.includes('중식') ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setFavoriteFoodCategories((prev) => prev.includes('중식') ? prev.filter((x) => x !== '중식') : [...prev, '중식'])}>중식</span>
-            <span className={`${boxStyle} ${favoriteFoodCategories.includes('양식') ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setFavoriteFoodCategories((prev) => prev.includes('양식') ? prev.filter((x) => x !== '양식') : [...prev, '양식'])}>양식</span>
-            <span className={`${boxStyle} ${favoriteFoodCategories.includes('분식') ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setFavoriteFoodCategories((prev) => prev.includes('분식') ? prev.filter((x) => x !== '분식') : [...prev, '분식'])}>분식</span>
-            <span className={`${boxStyle} ${favoriteFoodCategories.includes('일식') ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setFavoriteFoodCategories((prev) => prev.includes('일식') ? prev.filter((x) => x !== '일식') : [...prev, '일식'])}>일식</span>
+            {FOOD_CATEGORIES.map((category) => (
+              <span key={category} className={`${boxStyle} ${favoriteFoodCategories.includes(category as FoodList) ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setFavoriteFoodCategories((prev) => prev.includes(category as FoodList) ? prev.filter((x) => x !== category as FoodList) : [...prev, category as FoodList])}>{category}</span>
+            ))}
           </div>
         </div>
         {/* Cost */}
         <div className="w-full flex flex-col gap-2">
           <p className="text-black">데이트 비용은 얼마를 선호하시나요?</p>
           <div className="flex gap-2 w-full justify-between w-full">
-          <span className={`${boxStyle} ${dataCostPreference==='1만원 이하' ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setDataCostPreference('1만원 이하')}>1만원 이하</span>
-          <span className={`${boxStyle} ${dataCostPreference==='1 ~ 3만원' ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setDataCostPreference('1 ~ 3만원')}>1 ~ 3만원</span>
-          <span className={`${boxStyle} ${dataCostPreference==='3 ~ 5만원' ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setDataCostPreference('3 ~ 5만원')}>3 ~ 5만원</span>
-          <span className={`${boxStyle} ${dataCostPreference==='5 ~ 8만원' ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setDataCostPreference('5 ~ 8만원')}>5 ~ 8만원</span>
-          <span className={`${boxStyle} ${dataCostPreference==='8만원 이상' ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setDataCostPreference('8만원 이상')}>8만원 이상</span>
+            {COST_PREFERENCE.map((preference) => (
+              <span key={preference} className={`${boxStyle} ${dataCostPreference===preference ? "bg-[#93000A]/60 text-white" : "text-[#121920]"}`} onClick={() => setDataCostPreference(preference as CostList)}>{preference}</span>
+            ))}
           </div>
         </div>
         {/* Atmosphere */}
