44bb974027e4b0c0cd8e0c23560e8b55affa1044
yangjihun <yjhn0410@gmail.com>
2025-10-08 01:17:12 +0900

    feat: set up diary post API in diary/api - PIT-279

--- src/features/diary/api.ts
+++ src/features/diary/api.ts
@@ -2,6 +2,7 @@ import { api } from "../../shared/api/base";
 
 export const diaryApi = {
   getDiaryList: () => api.get('/api/diaries'),
+  createDiary: (data: any) => api.post('/api/diaries', data),
 };
 
 export const courseApi = {
