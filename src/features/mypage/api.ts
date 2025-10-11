ba3e55848cad6cc24c65b827246e8576cb804ff8
yangjihun <yjhn0410@gmail.com>
2025-10-12 01:28:05 +0900

    fix: update incorrect API endpoint path in mypage - PIT-488

--- src/features/mypage/api.ts
+++ src/features/mypage/api.ts
@@ -2,6 +2,6 @@ import { api } from "../../shared/api/base";
 
 export const mypageApi = {
   getMypage: () => api.get('/api/auth/mypage'),
-  putMypage: (data: any) => api.put('/api/auth/mypage', data),
+  putMypage: (data: any) => api.put('/api/auth/profile', data),
 };
 
