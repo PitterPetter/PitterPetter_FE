e69315ac5dd34eec22d8731b3e9db21b2b368273
yangjihun <yjhn0410@gmail.com>
2025-10-12 16:01:40 +0900

    fix: change POST data type to JSON in ValidateCouple - PIT-501

--- src/features/coupleroom/api.ts
+++ src/features/coupleroom/api.ts
@@ -3,5 +3,5 @@ import { PostCoupleRoom } from "./types";
 
 export const coupleRoomApi = {
   createCoupleRoom: (coupleRoom: PostCoupleRoom) => api.post('/api/couples/room', coupleRoom),
-  validateCoupleCode: (inviteCode: string) => api.post('/api/couples/match', inviteCode),
+  validateCoupleCode: (inviteCode: string) => api.post('/api/couples/match', {inviteCode: inviteCode}),
 };
