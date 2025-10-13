7322c70420816a78fdb6d6f16de02251aaa6f3f3
yangjihun <yjhn0410@gmail.com>
2025-10-13 09:04:58 +0900

    fix: change PUT mypage API in mypage - PIT-501

--- src/features/mypage/api.ts
+++ src/features/mypage/api.ts
@@ -3,7 +3,7 @@ import { api } from "../../shared/api/base";
 export const mypageApi = {
   getMypage: () => api.get('/api/auth/mypage'),
   putMypage: (data: any) => api.put('/api/auth/profile', data),
-  deleteCouple: (coupleCode: string) => api.delete(`/api/couples/room/${coupleCode}`),
+  deleteCouple: () => api.delete("/api/couples/cancel"),
   putCoupleHome: (data: any) => api.put('/api/couples', data),
 };
 
