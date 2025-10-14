9201ebce9f51980823d7e5ad8d8d52c9546e773c
yangjihun <yjhn0410@gmail.com>
2025-10-14 17:38:51 +0900

    feat: add POST rerecommend API in RecommendCoursePage - PIT-533

--- src/features/course/api.ts
+++ src/features/course/api.ts
@@ -2,3 +2,4 @@ import { api } from "../../shared/api/base";
 
 export const getCourseList = () => api.get("/api/courses");
 export const saveCourseApi = (course: any) => api.post("/api/courses", course);
+export const rerecommendCourseApi = (course: any) => api.post("/api/courses/recommends/replace", course);
