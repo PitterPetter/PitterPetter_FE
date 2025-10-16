6ec15b4822ff8d304f9b5107ef44746357f1b1fb
yangjihun <yjhn0410@gmail.com>
2025-10-16 18:04:47 +0900

    fix: change mock data in diary - PIT-586

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -100,8 +100,9 @@ export const handlers = [
     const totalPages = Math.ceil(totalElements / size);
     
     return HttpResponse.json({
-      success: true,
-      data: {
+      timestamp: "2025-10-16T13:48:30.178+09:00",
+      code: "COMMON200",
+      result: {
         content: paginatedDiaries,
         page: {
           page,
