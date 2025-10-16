789fccf0acc0f65ea111398b8047a8870d346eeb
yangjihun <yjhn0410@gmail.com>
2025-10-17 00:16:18 +0900

    style: adjust layout ratio on login page - PIT-592

--- src/features/mapbox/components/LoginMapbox.tsx
+++ src/features/mapbox/components/LoginMapbox.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useUIStore } from '../../../shared/store/ui.store';
-import { MapboxProps, MapRefs, TimeOfDay } from '../types';
+import { MapboxProps, MapRefs } from '../types';
 
 const Mapbox: React.FC<MapboxProps> = ({
   center = [127.1, 37.505],
@@ -12,13 +12,6 @@ const Mapbox: React.FC<MapboxProps> = ({
   const mapContainerRef = useRef<MapRefs['container']>(null);
   const mapRef = useRef<MapRefs['map']>(null);
   const { isMapReady, setMapReady } = useUIStore();
-  const getTimeOfDay = (date = new Date()): TimeOfDay => {
-    const hour = date.getHours();
-    if (hour >= 5 && hour < 9) return 'dawn';
-    if (hour >= 9 && hour < 17) return 'day';
-    if (hour >= 17 && hour < 21) return 'dusk';
-    return 'night';
-  }
 
   useEffect(() => {
     if (!mapContainerRef.current) return;
@@ -32,7 +25,7 @@ const Mapbox: React.FC<MapboxProps> = ({
       // 시간에 따라 조명 프리셋 변경
       config: {
         basemap: {
-          lightPreset: getTimeOfDay().toLowerCase() as 'dawn' | 'day' | 'dusk' | 'night',
+          lightPreset: 'day',
         }
       },
       center,
