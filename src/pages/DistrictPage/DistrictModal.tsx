5d5221880b5b2872b51645a9b0e81e361b1616ef
yangjihun <yjhn0410@gmail.com>
2025-10-19 17:54:53 +0900

    style: erase mapbox in DistrictModal - PIT-644

--- src/pages/DistrictPage/DistrictModal.tsx
+++ src/pages/DistrictPage/DistrictModal.tsx
@@ -1,12 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { LoginMapbox } from "../../features/mapbox";
 
 export const DistrictModal = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full z-50 bg-black bg-opacity-50">
-      <div className="absolute top-0 left-0 w-full h-[100vh] z-0 flex justify-center items-center overflow-hidden">
-        <LoginMapbox />
-      </div>
       <Outlet />
     </div>
   );
