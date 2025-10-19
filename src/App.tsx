a856f82bbf4da5ff7b58d726f32ca353af0ba158
yangjihun <yjhn0410@gmail.com>
2025-10-19 15:48:02 +0900

    feat: redirect to /login on default route acces - PIT-637

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { LoginPage } from './pages/LoginPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 // import { AuthInitializer } from './app/providers/AuthInitializer';
 import { MainPage } from './pages/MainPage';
 import { HeaderLayout } from './app/layouts';
@@ -34,6 +34,9 @@ function App() {
       <AuthBootstrap />
       <DataBootstrap />
       <Routes>
+        {/* 기본 경로를 로그인 페이지로 리다이렉트 */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        
         {/* 로그인 페이지 */}
         <Route path="/login" element={<LoginPage />} />
 
