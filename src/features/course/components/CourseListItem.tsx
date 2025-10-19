504eb0c22e7a6672b05f2c50ecfecc37571aec1a
yangjihun <yjhn0410@gmail.com>
2025-10-20 04:40:50 +0900

    style: fix course item round degree in CourseListItem - PIT-644

--- src/features/course/components/CourseListItem.tsx
+++ src/features/course/components/CourseListItem.tsx
@@ -4,10 +4,10 @@ import type { Course } from "../types";
 export function CourseListItem({ course }: { course: Course }) {
   const navigate = useNavigate();
   return (
-    <div className="relative flex flex-col h-[320px] bg-white border border-gray-200 rounded-[20px] cursor-pointer hover:shadow-sm transition-shadow duration-200"
+    <div className="group relative flex flex-col h-[320px] bg-white border border-gray-200 rounded-[12px] transition-all duration-200 cursor-pointer hover:shadow-sm transition-shadow duration-200"
       onClick={() => { navigate(`/course/${course.course_id}`); }}
     >
-      <div className="h-20 bg-primary rounded-t-[20px] flex items-end px-4 pb-3">
+      <div className="h-16 bg-primary rounded-t-[11px] flex items-end px-4 pb-3 group-hover:bg-primary/80 transition-all duration-200">
         <h2 className="text-white font-medium text-lg">{course.title}</h2>
       </div>
       
